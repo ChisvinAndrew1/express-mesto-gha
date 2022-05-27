174739567444c82723794eb3d6987cf233f2ccd6
ChisvinAndrew1 <frolik947@gmail.com>
2022-05-28 01:23:10 +0300

    feat/gha-server4

--- midllewars/errorHandler.js
+++ midllewars/errorHandler.js
@@ -1,7 +1,7 @@
 module.exports.errorHandler = (err, _req, res, next) => {
-  res.status(err.statusCode).send({
+  res.status(err.statusCode).send(JSON.stringify({
     message: err.message,
     code: err.statusCode,
-  });
+  }));
   next();
 };
