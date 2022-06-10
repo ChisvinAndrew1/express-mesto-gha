c0be7ceed2a9a9308493ea295bc05302d2e99d33
ChisvinAndrew1 <frolik947@gmail.com>
2022-06-10 23:19:53 +0300

    feat/jwt2

--- routers/users.js
+++ routers/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUserById, getMeInfo, updateProfile, updateProfileAvatar,
 } = require('../controllers/users');
@@ -6,9 +7,22 @@ const {
 router.get('/', getUsers);
 router.get('/me', getMeInfo);
 
-router.get('/:id', getUserById);
-// router.post('/users', createUser);
-router.patch('/me/avatar', updateProfileAvatar);
+router.get('/:id', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex().required(),
+  }),
+}), getUserById);
 
-router.patch('/me', updateProfile);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().pattern(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/).required(),
+  }),
+}), updateProfileAvatar);
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateProfile);
 module.exports = router;
